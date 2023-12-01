b3e76c0b496bdfc42e020d1e1d22090841397c6b
blumj808 <blumenfeld.jake@gmail.com>
2023-12-01 16:13:36 +0000

    save

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,8 +1,9 @@
 class DogsController < ApplicationController
   def index
-    matching_dogs = Dog.all
+    @list_of_dogs = Dog.all.order({ :created_at => :desc })
+    #matching_dogs = Dog.all
 
-    @list_of_dogs = matching_dogs.order({ :created_at => :desc })
+    #@list_of_dogs = matching_dogs.order({ :created_at => :desc })
 
     render({ :template => "dogs/index" })
   end
@@ -19,21 +20,22 @@ class DogsController < ApplicationController
 
   def create
     the_dog = Dog.new
-    the_dog.name = params.fetch("query_name")
-    the_dog.breed = params.fetch("query_breed")
-    the_dog.age = params.fetch("query_age")
-    the_dog.bio = params.fetch("query_bio")
-    the_dog.shelter_id = params.fetch("query_shelter_id")
-    the_dog.gender = params.fetch("query_gender")
-    the_dog.owner_id = params.fetch("query_owner_id")
-    the_dog.picture = params.fetch("query_picture")
-    the_dog.status = params.fetch("query_status")
+    the_dog.name = params.fetch("name")
+    the_dog.breed = params.fetch("breed")
+    the_dog.age = params.fetch("age")
+    the_dog.bio = params.fetch("bio")
+    the_dog.shelter_id = params.fetch("shelter_id", nil)
+    the_dog.gender = params.fetch("gender")
+    the_dog.owner_id = params.fetch("owner_id", nil)
+    the_dog.picture = params.fetch("picture",nil)
+    the_dog.status = params.fetch("status")
 
     if the_dog.valid?
       the_dog.save
       redirect_to("/dogs", { :notice => "Dog created successfully." })
     else
-      redirect_to("/dogs", { :alert => the_dog.errors.full_messages.to_sentence })
+      puts the_dog.errors.full_messages
+      redirect_to("/dogs", { :alert => "Dog could not be created." })
     end
   end
 
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -16,6 +16,6 @@
 #  shelter_id :integer
 #
 class Dog < ApplicationRecord
-  belongs_to :shelter, class_name: "Shelter", foreign_key: "shelter_id", counter_cache: true
-  belongs_to :owner, class_name: "Owner", foreign_key: "owner_id", counter_cache: true
+  belongs_to :shelter, class_name: "Shelter", foreign_key: "shelter_id", counter_cache: true, optional: true
+  belongs_to :owner, class_name: "Owner", foreign_key: "owner_id", counter_cache: true, optional: true
 end
