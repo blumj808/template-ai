e9a6ffff820b1d5070a3dd0064301233d1076af7
blumj808 <blumenfeld.jake@gmail.com>
2023-12-07 21:26:54 +0000

    save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,18 @@ class UsersController < ApplicationController
 
   def create
     role = params['role'].present? ? params['role'] : User::GUEST_ROLE
-    @user = User.new({ :username => params['username'], :password => params['password'], :role => role })
+    @user = User.new({
+      :username => params['user']['username'],
+      :password => params['user']['password'],
+      :password_confirmation => params['user']['password_confirmation'],
+      :role => role
+    })
+    
     if @user.save
       session.store(:user_id, @user.id)
-      redirect_to("/",notice: "Account created successfully!")
+      redirect_to("/", :notice => "Account created successfully!")
     else
-      render :new
+      render({ :template => "users/new" })
     end
   end
 end
